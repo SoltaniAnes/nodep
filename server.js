db19cb80d28984060ad9c5e5475367b1c3463fce
anessoltani <soltanianes@gmail.com>
2024-04-27 16:28:45 +0200

    Adding the Auth page

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express=require("express");
+const path= require('path');
 const router=require("./routes");
+const mustache=require('mustache-express');
 const app=express();
+
+app.engine('html', mustache());
+app.set('view engine', 'html');
+app.set('views','./views');
+
+app.use(express.static(path.join(__dirname, '/public')));
 app.use("/",router);
 app.listen(3000)
